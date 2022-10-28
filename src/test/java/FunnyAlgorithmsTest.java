2a2810b238c6b9b64fa01ee6027ed37c442a19c2
PierKnight <pier.altimari@libero.it>
2022-10-28 16:38:29 +0200

    implemented selection sort test

--- src/test/java/FunnyAlgorithmsTest.java
+++ src/test/java/FunnyAlgorithmsTest.java
@@ -60,10 +60,62 @@ public class FunnyAlgorithmsTest {
     @Test
     public void isBinarySearchWorking()
     {
-        assertEquals(0, this.funnyAlgorithms.binarySearch(new int[]{3,8,1,2,3,5,7}, 3));
+        assertEquals("FAILED CHECK SIMPLE MIDDLE",2, this.funnyAlgorithms.binarySearch(new int[]{2,7,11,15}, 11));
+
+        assertEquals("FAILED CHECK SIMPLE START",0, this.funnyAlgorithms.binarySearch(new int[]{2,7,11,15}, 2));
+        assertEquals("FAILED CHECK SIMPLE END",3, this.funnyAlgorithms.binarySearch(new int[]{2,7,11,15}, 15));
 
         //check if it finds the middle one
-        assertEquals(2, this.funnyAlgorithms.binarySearch(new int[]{1,1,1,1,1}, 1));
+        assertEquals("FAILED DUPLICATE MIDDLE FIND",2, this.funnyAlgorithms.binarySearch(new int[]{1,1,1,1,1}, 1));
+
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void acceptOnly0And1()
+    {
+        this.funnyAlgorithms.selectionSort(new int[]{}, -5);
+        this.funnyAlgorithms.selectionSort(new int[]{}, 3);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void checkWithNullArray()
+    {
+        this.funnyAlgorithms.selectionSort(null, 0);
+    }
+    /**
+     * Check If Array is sorted
+     *
+     * @param array Array to be check
+     * @param order Direction to sort the array (0:Ascending, 1:Descending)
+     */
+    private boolean isArraySorted(int[] array, int order)
+    {
+        if(array.length == 0)
+            return true;
+
+        int last = array[0];
+
+        for(int i = 1;i < array.length;i++)
+        {
+            if(order == 1 ? array[i] < last : array[i] > last)
+                return false;
+            last = array[i];
+        }
+        return true;
+    }
+
+    @Test
+    public void checkMergeSort()
+    {
+        int[] arrayToSortAscending = new int[]{5,21,2,3,7,3,1,8,4,3,7};
+        int[] arrayToSortDescending = new int[]{5,21,2,3,7,3,1,8,4,3,7};
+
+        this.funnyAlgorithms.selectionSort(arrayToSortAscending,0);
+        assertTrue(isArraySorted(arrayToSortAscending, 0));
+
+        this.funnyAlgorithms.selectionSort(arrayToSortDescending,1);
+        assertTrue(isArraySorted(arrayToSortDescending, 1));
 
     }
 
